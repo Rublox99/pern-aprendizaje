4492813edf17cf91786eedd11e17b1a0b104260c
Ruben Almendares <camiloalmendarez47@gmail.com>
2023-09-25 15:55:32 -0600

    UPDATE - Agregado taskRoutes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@
 const express= require("express")
 const morgan= require("morgan")
 
+//EXPORTA LAS RUTAS DEFINIDAS COMO ENDPOINTS
+const taskRoutes= require('./routes/tasks.routes')
+
 //CREA UNA INSTANCIA DE APLICACIÓN EXPRESS
 const app= express()
 
 //VER EN CONSOLA LAS PETICIONES
 app.use(morgan('dev'))
+
+//
+app.use(taskRoutes)
 
 app.listen(3000)
 console.log("Server Port: 3000")
